0c16e79751136fce2e4d5ae0a841339bb6a56190
JustOnTime <info@justontime.co>
2017-11-03 19:16:26 +0200

    fix release playbook #8

---
+++ release/filter_plugins/get_next_weekday.py
@@ -0,0 +1,21 @@
+#*******************************************************************************
+# Licensed Materials - Property of L.M.B.-Consulting Ltd.
+# (c) Copyright L.M.B.-Consulting 2016. All Rights Reserved.
+#*******************************************************************************
+import datetime
+
+def get_next_weekday(startdate, weekday):
+    """
+    @startdate: given date, in format '2013-05-25T08:00:00Z'
+    @weekday: week day as a integer, between 0 (Monday) to 6 (Sunday)
+    """
+    d = datetime.datetime.strptime(startdate, "%Y-%m-%dT%H:%M:%SZ")
+    t = datetime.timedelta((7 + weekday - d.weekday()) % 7 + 7)
+    return (d + t).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+class FilterModule(object):
+
+    def filters(self):
+        return {
+            'get_next_weekday': get_next_weekday
+        }
