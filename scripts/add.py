24fb8e242ca41d828b138104eef037af2d299816
JustOnTime <info@justontime.co>
2017-11-10 16:21:55 +0200

    Import LinkedIn Connections to MainChimp list

---
+++ scripts/add.py
@@ -0,0 +1,34 @@
+import csv
+import mailchimp3
+import json
+
+def main():
+
+    with open('.mailchimp.json') as json_data_file:
+        data = json.load(json_data_file)
+
+    with open(data['csv_file'], 'rb') as csvfile:
+        connections = csv.DictReader(csvfile)
+        for connection in connections:
+
+            list_id = data['list_id']
+            client = mailchimp3.MailChimp(data['user'],data['secret'])
+
+            try:
+
+                client.lists.members.create(list_id, {
+                'email_address': connection['Email Address'],
+                'status': 'subscribed',
+                'merge_fields': {
+                    'FNAME': connection['First Name'],
+                    'LNAME': connection['Last Name']
+                }})
+                print(connection['First Name'], connection['Last Name'], connection['Email Address'])
+
+            except Exception as e:
+                pass
+
+    return
+
+if __name__ == "__main__":
+    main()
