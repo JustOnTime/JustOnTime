0d483dffa7bfec761b868751918ede9dc2e0c174
John Doe <info@justontime.co>
2018-06-01 19:52:42 +0300

    Register to mailing list #15

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 application = Flask(__name__)
 
-@application.route('/')
+@application.route('/', methods=['GET','POST'])
 def index():
+    if request.method == 'POST':
+        with open("mailinglist.txt","a+") as ml:
+            ml.write(request.form['email'] + "\n")
     return render_template('index.html')
 
 if __name__ == "__main__":
