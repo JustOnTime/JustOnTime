1e77e3f2652774d03e7f721d75004cc1cf516597
John Doe <info@justontime.co>
2018-06-07 08:43:39 +0300

    local database #17

--- application.py
+++ application.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template, request, send_from_directory
+import MySQLdb
 
 application = Flask(__name__, static_folder='static')
 
 @application.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        with open("mailinglist.txt","a+") as ml:
-            ml.write(request.form['email'] + "\n")
+        conn = MySQLdb.connect(host= "localhost",
+                          user="root",
+                          passwd="12345",
+                          db="justontime")
+        x = conn.cursor()
+        try:
+           x.execute("""INSERT INTO list VALUES (%s)""",(request.form['email']))
+           conn.commit()
+        except:
+           conn.rollback()
+
+        conn.close()
+
     return render_template('index.html')
 
 @application.route('/robots.txt')
@@ -15,4 +27,4 @@ def static_from_root():
     return send_from_directory(application.static_folder, request.path[1:])
 
 if __name__ == "__main__":
-    application.run()
+    application.run(host='0.0.0.0')
